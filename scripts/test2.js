f9d7a45de0748413b4717f243a2fa4a5759b7ce5
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-16 06:35:11 +0200

    Update test2.js

--- scripts/test2.js
+++ scripts/test2.js
@@ -17,11 +17,13 @@ document.write("<p>" + (false == "false") + "</p>");
 // let name = prompt("What is your name?", "test");
 // alert(name);
 
-let num = prompt("Enter a number", "");
-if (num > 0) {
-  alert(1);
-} else if (num < 0) {
-  alert(-1);
-} else {
-  alert(0);
-}
+// let num = prompt("Enter a number", "");
+// if (num > 0) {
+//   alert(1);
+// } else if (num < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+alert(1 || 0);
