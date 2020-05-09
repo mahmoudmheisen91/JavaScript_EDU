8ef0ca7c5b7d61478bf9babcf11c38eaa0c4c589
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-05-09 03:07:12 +0300

    add is prime function

---
+++ scripts/test3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let a = "px" + 9;
+let b = 9 + "test";
+
+console.log(a);
+console.log(b);
+
+const isPrime = (x) =>
+  x > 1 &&
+  ![...Array(Math.floor(Math.sqrt(x)) - 1)].some((z, y) => x % (y + 2) === 0);
+
+console.log([...Array(9)]);
+console.log(isPrime(27));
